7e35531969b510c54b4035007186a4ccb894df84
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-26 23:29:13 -0300

    edited constants content

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,15 @@
 import {} from "../assets"
+
+export const heroHeading = [
+  {
+    id: "heading",
+    content: "Build The Community Your Fans Will Love"
+  },
+];
+
+export const heroText = [
+  {
+    id : "text",
+    content : "Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with. your users as you engage in genuine discussion"
+  },
+];
