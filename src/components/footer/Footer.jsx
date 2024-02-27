822574715ac21a34293a53b7e264226d294b1711
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-27 13:14:03 -0300

    added styles for the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import {FaFacebookF, FaTwitter, FaInstagram} from 'react-icons/fa';
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='huddle__footer section__padding'>
+      <div className='huddle__footer-icons__container'>
+        <div className='huddle__footer-icons'>
+          <a href="#">< FaFacebookF/></a>
+          <a href="#">< FaTwitter /></a>
+          <a href="#">< FaInstagram /></a>
+        </div>
+      </div>
+    </div>
   )
 }
 
