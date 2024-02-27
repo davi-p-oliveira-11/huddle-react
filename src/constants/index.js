b289e11f29c4648a1c36871fd37b37af0953f7a2
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-27 12:55:34 -0300

    fixed heroData export

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,9 @@
-const heroData = [
+const heroData = 
   {
     title: "Build The Community Your Fans Will Love",
     description: "Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion",
     buttonText: "Register"
-  },
-];
+  }
 
 export default heroData;
 
